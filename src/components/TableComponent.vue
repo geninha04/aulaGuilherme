80a70dcb4e5860148c4c4b2c9d3b32aea1e977dd
Manoela <geninha04@gmail.com>
2022-05-20 16:25:25 -0300

    aula guilherme

---
+++ src/components/TableComponent.vue
@@ -0,0 +1,50 @@
+<template>
+  <section>
+    <div class='tbl-header'>
+      <table cellpadding='0' cellspacing='0' border='0'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Course</th>
+            <th>Rating</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+    <div class='tbl-content'>
+      <table cellpadding='0' cellspacing='0' border='0'>
+        <tbody>
+          <tr>
+            <td>Estudante 1</td>
+            <td>Administração</td>
+            <td>9</td>
+            <td>
+              <button class='editButton'>Edit</button>
+              <button class='deleteButton'>Delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </section>
+  <div>
+    <br/>
+    <h2>Ops, no data found :(</h2>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "TableComponent",
+  props:{},
+  emits:[],
+  setup(){}
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/MainPage.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <section class="structure">
+      <div><h1>Students Database</h1>
+      <br/>
+    </div>
+  <div> 
+      <form>
+        <input placeholder="Student Name"/>
+        <input placeholder="Couse"/>
+        <input placeholder="Rating"/>
+        <button>Add</button>
+        <button class = "resetButton">Reset</button>
+        <button>Save</button>
+        <button class = "resetButton">Cancel</button>
+      </form>
+  </div>
+  <div>
+    <TableComponent/>
+  </div>
+    </section>
+  </div>
+
+
+</template>
+
+<script>
+import TableComponent from "../components/TableComponent.vue"
+
+export default {
+name: "MainPage",
+components: {
+  TableComponent
+},
+setup(){
+
+}
+}
+</script>
+
+<style>
+
+</style>
